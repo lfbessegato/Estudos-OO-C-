76661c2725975a473e03393744df0f45909f723e
Lfbessegato <Lfrbessegato@gmail.com>
2021-05-05 19:35:40 -0300

    Capitulo 07 - Acesso a Banco de dados

---
+++ Capitulo07/Modelo/Disciplina.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Disciplina
+    {
+        public long? DisciplinaID { get; set; }
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj is Disciplina)
+            {
+                Disciplina d = obj as Disciplina;
+                return this.DisciplinaID.Equals(d.DisciplinaID);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (11 + this.DisciplinaID == null ? 0 : this.DisciplinaID.GetHashCode());
+        }
+    }
+}
